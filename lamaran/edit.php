b022b8b12ade20dfa5a23c9ebedfce621cc34470
2406033-Faisal <2406033@itg.ac.id>
2026-06-18 20:42:38 +0700

    Update edit.php

--- lamaran/edit.php
+++ lamaran/edit.php
@@ -2,7 +2,7 @@
 require_once '../config/database.php';
 require_once '../config/session.php';
 
-requireRole('kandidat');
+requireRole('kandidat').
 
 $id_lamaran = sanitize($_GET['id'] ?? '');
 if (empty($id_lamaran)) {
